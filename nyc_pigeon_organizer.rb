313ad9872992203c55c890db5bd8f93b158d9a2c
Ange Batie <ange.batie@gmail.com>
2020-08-20 02:58:23 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -5,5 +5,8 @@ def nyc_pigeon_organizer(data)
   final_results = data.each_with_object({}) do |(key, value), array|
     value.each do |inner_key, names|
       names.each do |name|
-        
+        array.uniq 
+      end
+    end
+    array[name][key].push(inner_key.to_s)
 end
