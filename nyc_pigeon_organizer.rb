3449e3f8d218644a7b90a93cde57f916ce8c92dd
Ange Batie <ange.batie@gmail.com>
2020-08-20 03:08:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -2,12 +2,10 @@ require 'pry'
 
 def nyc_pigeon_organizer(data)
   # write your code here!
-  final_results = data.each_with_object({}) do |(key, value), array|
-    value.each do |inner_key, name|
+  new_hash = {}
+  data.each do |key, value|
+    value.each do |value2, names|
       names.each do |name|
-        array.uniq 
-      end
-    end
-    array[name][key].push(inner_key.to_s)
-end
+        
+        
 end
