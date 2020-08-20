3c1ec37339d15aca1b3f8767dd60425f938e559b
Ange Batie <ange.batie@gmail.com>
2020-08-20 02:46:36 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,5 +3,5 @@ require 'pry'
 def nyc_pigeon_organizer(data)
   # write your code here!
   data.each_with_object({}) do | (key, value), array |
-    binding.pry
+    value.each do | inner_key, name |
 end
