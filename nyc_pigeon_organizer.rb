16ded94bb69735c2cef667472b204768c5bcc0a8
Ange Batie <ange.batie@gmail.com>
2020-08-20 03:13:45 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -7,7 +7,7 @@ def nyc_pigeon_organizer(data)
     value.each do |value2, names|
       names.each do |name|
         
-        data.uniq 
+        new_hash.uniq 
         new_hash[name] = {}
         new_hash[name][key] = []
       end
